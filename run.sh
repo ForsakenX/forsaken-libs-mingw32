bad1a911231e4c20554e0db45dd521e064cd7af0
Daniel Aquino <mr.danielaquino@gmail.com>
2017-05-31 01:51:42 -0400

    run.sh: get rid of `cd` error for new expected layouts

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
 cd "$(dirname -- "$0")/.."
-skeleton="$PWD/../skeleton"
+
+##
+# this is the old layout that we don't really use anymore
+# if you want to specify a specific target directory override this
+# otherwise we'll assume you're using your Forsaken folder as
+# your game directory as well
+##
+: ${SKELETON:="$PWD/../skeleton"}
+[[ -d $SKELETON ]] && cd "$skeleton"
+
 projectx="$PWD/projectx"
-cd "$skeleton"
 wine "$projectx" "$@"
