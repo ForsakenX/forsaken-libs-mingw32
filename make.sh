05af1231da9afbafa047310dfd26ceeee5660a59
Daniel Aquino <mr.danielaquino@gmail.com>
2017-05-31 01:49:58 -0400

    make.sh: add new mingw64-gcc binary to default list

--- make.sh
+++ make.sh
@@ -8,7 +8,12 @@ export PKG_CONFIG_PATH="$PWD/mingw/lib/pkgconfig"
 function regex_match { [[ $1 =~ $2 ]]; }
 flags=""
 if ! regex_match "$*" "\bCC="; then
-	versions="i386-mingw32-gcc i586-mingw32-gcc i586-mingw32msvc-gcc i686-mingw32-gcc"
+	versions="
+		i386-mingw32-gcc
+		i586-mingw32-gcc i586-mingw32msvc-gcc
+		i686-mingw32-gcc
+		i686-w64-mingw32-gcc
+	"
 	cc=$(which $versions 2>/dev/null | tail -n 1)
 	if [[ $cc ]]; then
 		flags="CC=$cc"
